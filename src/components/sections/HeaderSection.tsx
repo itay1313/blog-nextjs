4c679916f690cead67832bc365b586548ea1d60c
itay haephrati <itay1313@gmail.com>
2022-10-08 21:56:46 +0300

    fix: fixed alerts

--- src/components/sections/HeaderSection.tsx
+++ src/components/sections/HeaderSection.tsx
@@ -3,7 +3,9 @@ import * as React from 'react';
 import Button from '@/components/buttons/Button';
 import UnstyledLink from '@/components/links/UnstyledLink';
 
+import HamburgerIcon from '~/svg/HamburgerIcon.svg';
 import Logo from '~/svg/Logo.svg';
+import SearchIcon from '~/svg/SearchIcon.svg';
 
 const topmenulinks = [
   { href: '/', label: 'Product' },
@@ -47,17 +49,7 @@ export default function HeaderSection() {
           <Button className='ml-4'>Sign up for free</Button>
         </div>
         <button className='menu md:hidden' title='Open Site Menu'>
-          <svg
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-            stroke-width='2'
-          >
-            <line x1='5' y1='6' x2='19' y2='6'></line>
-            <line x1='5' y1='12' x2='19' y2='12'></line>
-            <line x1='5' y1='18' x2='19' y2='18'></line>
-          </svg>
+          <HamburgerIcon />
         </button>
       </div>
 
@@ -81,21 +73,7 @@ export default function HeaderSection() {
           <div className='flex items-center'>
             <div className='relative my-auto flex items-center rounded-md shadow-sm'>
               <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
-                <svg
-                  className='SearchIcon'
-                  width='24'
-                  height='24'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                >
-                  <path
-                    d='M19.2307 19.2307L15.4845 15.4845L19.2307 19.2307ZM4.74525 10.989C4.74525 7.53246 7.53246 4.74525 10.989 4.74525C14.4355 4.74525 17.2327 7.53246 17.2327 10.989C17.2327 14.4355 14.4355 17.2327 10.989 17.2327C7.53246 17.2327 4.74525 14.4355 4.74525 10.989Z'
-                    stroke='currentColor'
-                    stroke-width='1.4985'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                  ></path>
-                </svg>
+                <SearchIcon />
               </div>
               <input
                 type='text'
@@ -111,17 +89,7 @@ export default function HeaderSection() {
           </div>
         </div>
         <button className='menu md:hidden' title='Open Site Menu'>
-          <svg
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-            stroke-width='2'
-          >
-            <line x1='5' y1='6' x2='19' y2='6'></line>
-            <line x1='5' y1='12' x2='19' y2='12'></line>
-            <line x1='5' y1='18' x2='19' y2='18'></line>
-          </svg>
+          <HamburgerIcon />
         </button>
       </div>
     </header>
