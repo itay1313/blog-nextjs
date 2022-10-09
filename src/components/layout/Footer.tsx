fe79da10dc390c2b9fcf90c81de5b71555a8f0f2
itay haephrati <itay1313@gmail.com>
2022-10-09 21:24:41 +0300

    refactor: footerlinks

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -13,17 +13,46 @@ const productLinks = [
   { href: '/', label: 'For MSSPs' },
   { href: '/', label: 'Pricing' },
 ];
+
 const casesLinks = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/', label: 'Threat intelligence enrichment' },
+  { href: '/', label: 'Phishing attack response' },
+  { href: '/', label: 'Suspicious login alerts' },
+  { href: '/', label: 'Vulnerability management' },
+  { href: '/', label: 'Endpoint detection and response' },
+  { href: '/', label: 'Abuse response' },
+  { href: '/', label: 'Fraud analysis and response' },
+  { href: '/', label: 'Slack bots' },
+  { href: '/', label: 'Employee onboarding' },
 ];
+
 const resourcesLinks = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/', label: 'Webinars' },
+  { href: '/', label: 'Blog' },
+  { href: '/', label: 'Podcast' },
+  { href: '/', label: 'Customer stories' },
+  { href: '/', label: 'Tines Hub' },
+  { href: '/', label: 'Story library' },
+  { href: '/', label: 'Docs' },
+  { href: '/', label: 'API' },
+  { href: '/', label: 'Lessons' },
+  { href: '/', label: "What's new" },
 ];
+
 const companyLinks = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/', label: 'About' },
+  { href: '/', label: 'Upcoming events' },
+  { href: '/', label: 'Careers' },
+  { href: '/', label: 'Customer love' },
+  { href: '/', label: 'Partnerships' },
+  { href: '/', label: 'Newsroom' },
+  { href: '/', label: 'Contact' },
+  { href: '/', label: 'Terms' },
+  { href: '/', label: 'Privacy notice' },
+  { href: '/', label: 'Sub-processors' },
+  { href: '/', label: 'Security' },
+  { href: '/', label: 'Data processing' },
+  { href: '/', label: 'GDPR request' },
 ];
 
 export default function Footer() {
