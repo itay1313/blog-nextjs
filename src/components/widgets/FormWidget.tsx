1aec5acce9eb022e07ffa00b5cd299ce8b0d8308
itay haephrati <itay1313@gmail.com>
2022-10-11 00:45:54 +0300

    refactor: form validation

--- src/components/widgets/FormWidget.tsx
+++ src/components/widgets/FormWidget.tsx
@@ -1,15 +1,16 @@
-import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import SuccessIcon from '~/svg/SuccessIcon.svg';
+
+type Inputs = {
+  email_address: string;
+};
 
 // FormWidget component
 
 const FormWidget = () => {
-  const router = useRouter();
-  const confirmationScreenVisible =
-    router.query?.success && router.query.success === 'true';
-  const formVisible = !confirmationScreenVisible;
+  const [formSubmit, setFormSubmit] = useState(false);
 
   const ConfirmationMessage = (
     <div
@@ -21,94 +22,101 @@ const FormWidget = () => {
     </div>
   );
 
-  // const ErrorMessage = (
-  //   <div
-  //     className='flex gap-2'
-  //     style={{ backgroundColor: 'rgba(255,255,255,0.1)' }}
-  //   >
-  //     <SuccessIcon />
-  //     <p>Please enter a valid email address.</p>
-  //   </div>
-  // );
-
-  const ContactForm = (
-    <form
-      className='container'
-      method='POST'
-      name='contact-form'
-      action='contact/?success=true'
-      data-netlify='true'
-      data-netlify-honeypot='bot-field'
+  const ErrorMessage = (
+    <div
+      className='flex gap-2'
+      style={{ backgroundColor: 'rgba(255,255,255,0.1)' }}
     >
-      <div>
-        <div className='py-5'>
-          <div className='grid grid-cols-6'>
-            <div className='col-span-6 sm:col-span-3'>
-              <input
-                type='text'
-                name='email-address'
-                id='email-address'
-                className='input-style rounded-tl-2xl border-b-0 for-mobile:rounded-tr-2xl'
-                required
-                placeholder='Your email *'
-              />
-            </div>
-
-            <div className='col-span-6 sm:col-span-3'>
-              <input
-                type='text'
-                name='company'
-                id='company'
-                className='input-style rounded-tr-2xl border-l-0 border-b-0 for-mobile:rounded-tr-none for-mobile:border-l-2 for-mobile:border-b-2'
-                placeholder='Your company name'
-              />
-            </div>
-            <div className='col-span-6 sm:col-span-3'>
-              <input
-                type='text'
-                name='first-name'
-                id='first-name'
-                className='input-style rounded-bl-2xl for-mobile:rounded-bl-none for-mobile:border-y-0 for-mobile:border-l-2'
-                placeholder='First name'
-              />
-            </div>
-
-            <div className='col-span-6 sm:col-span-3'>
-              <input
-                type='text'
-                name='last-name'
-                id='last-name'
-                className='input-style rounded-br-2xl border-l-0 for-mobile:rounded-bl-2xl for-mobile:border-l-2'
-                placeholder='Last name'
-              />
-            </div>
-          </div>
-          <div className='pt-4 for-mobile:text-center'>
-            <button type='submit' className='form-button'>
-              Subscribe
-            </button>
-          </div>
-          <div>
-            <p className='mt-[1.125rem] text-sm text-light for-mobile:text-center'>
-              By filling out this form you agree to the terms and conditions in
-              our{' '}
-              <a
-                href='/privacy'
-                target='_blank'
-                className='font-bold underline'
-              >
-                Privacy&nbsp;Notice
-              </a>{' '}
-              <sup>↗</sup>.
-            </p>
-          </div>
-          {ConfirmationMessage}
-        </div>
-      </div>
-    </form>
+      <SuccessIcon />
+      <p>Please enter a valid email address.</p>
+    </div>
   );
 
-  return <>{formVisible ? ContactForm : ConfirmationMessage}</>;
+  const ContactForm = () => {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = () => setFormSubmit(true);
+
+    return (
+      <form className='container' onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <div className='py-5'>
+            <div className='grid grid-cols-6'>
+              <div className='col-span-6 sm:col-span-3'>
+                <input
+                  type='text'
+                  id='email-address'
+                  className='input-style rounded-tl-2xl border-b-0 for-mobile:rounded-tr-2xl'
+                  placeholder='Your email *'
+                  {...register('email_address', {
+                    required: true,
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'invalid email address',
+                    },
+                  })}
+                />
+              </div>
+
+              <div className='col-span-6 sm:col-span-3'>
+                <input
+                  type='text'
+                  name='company'
+                  id='company'
+                  className='input-style rounded-tr-2xl border-l-0 border-b-0 for-mobile:rounded-tr-none for-mobile:border-l-2 for-mobile:border-b-2'
+                  placeholder='Your company name'
+                />
+              </div>
+              <div className='col-span-6 sm:col-span-3'>
+                <input
+                  type='text'
+                  name='first-name'
+                  id='first-name'
+                  className='input-style rounded-bl-2xl for-mobile:rounded-bl-none for-mobile:border-y-0 for-mobile:border-l-2'
+                  placeholder='First name'
+                />
+              </div>
+
+              <div className='col-span-6 sm:col-span-3'>
+                <input
+                  type='text'
+                  name='last-name'
+                  id='last-name'
+                  className='input-style rounded-br-2xl border-l-0 for-mobile:rounded-bl-2xl for-mobile:border-l-2'
+                  placeholder='Last name'
+                />
+              </div>
+            </div>
+            <div className='pt-4 for-mobile:text-center'>
+              <button type='submit' className='form-button'>
+                Subscribe
+              </button>
+            </div>
+            <div>
+              <p className='mt-[1.125rem] text-sm text-light for-mobile:text-center'>
+                By filling out this form you agree to the terms and conditions
+                in our{' '}
+                <a
+                  href='/privacy'
+                  target='_blank'
+                  className='font-bold underline'
+                >
+                  Privacy&nbsp;Notice
+                </a>{' '}
+                <sup>↗</sup>.
+              </p>
+            </div>
+            {errors.email_address && ErrorMessage}
+          </div>
+        </div>
+      </form>
+    );
+  };
+
+  return <>{!formSubmit ? <ContactForm /> : ConfirmationMessage}</>;
 };
 
 export default FormWidget;
